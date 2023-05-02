ad42db7a3feff491734911cc6fcd5fedf2e30ce5
Andrii-Sh <114801650+Andrii-Sh@users.noreply.github.com>
2023-05-02 16:32:57 +0300

    completed 2 steps of task 02

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import Feedback from './feedback/Feedback';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        // height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Feedback />
     </div>
   );
 };
